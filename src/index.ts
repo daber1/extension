7aadf6ccf8938976086aca4379b58973e4b33ae6
daber1 <v.lopatovskii@innopolis.university>
2024-10-17 09:45:11 +0300

    Added TO-DO functionality

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,17 @@ import {
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
 
-import { ICommandPalette, MainAreaWidget } from '@jupyterlab/apputils';
+import {
+  INotebookTracker
+} from '@jupyterlab/notebook';
 
-import { Widget } from '@lumino/widgets';
+import { NotebookPanel } from '@jupyterlab/notebook';
+import { Notification } from '@jupyterlab/apputils';
+
+// Create a new command for your extension
+const TODO_COMMAND: string = 'jupyterlab_todo:check-todos';
+
+
 
 /**
  * Initialization data for the jupyterlab_todo extension.
@@ -14,38 +22,52 @@ const plugin: JupyterFrontEndPlugin<void> = {
   id: 'jupyterlab_todo',
   description: 'Pop up your to-do\'s in opened notebook.',
   autoStart: true,
-  requires: [ICommandPalette],
-  activate: (app: JupyterFrontEnd, palette:ICommandPalette) => {
+  requires: [INotebookTracker],
+  activate: (app: JupyterFrontEnd, tracker: INotebookTracker) => {
     console.log('JupyterLab extension jupyterlab_todo is activated!');
-    
+    const checkForTodos = async () => {
+      // Open notebook
+      const currentNotebook = tracker.currentWidget as NotebookPanel;
+      if (!currentNotebook.context.contentsModel?.name) return;
+      // const cellList = currentNotebook.content.widgets;
+      const cellList = currentNotebook.content.model?.cells
+      if (!cellList) return;
 
-    // A widget creator function
-    const newWidget = () => {
-      const content = new Widget();
-      const widget = new MainAreaWidget({ content });
-      widget.id = 'todo-jupyterlab';
-      widget.title.label = 'Random Picture';
-      widget.title.closable = true;
-      return widget;
+      for (let index = 0; index < cellList.length; index++){
+        const cell = cellList.get(index);
+        const cellJson = cell.toJSON();
+        const cellText = Array.isArray(cellJson.source) ? cellJson.source.join('\n') : cellJson.source; 
+        const todoMatches = cellText.match(/TODO:|todo:|To do|ToDo/gi);
+        if (todoMatches && todoMatches.length > 0) {
+          console.log(cellJson);
+          Notification.emit(`Found TODOs in the current notebook`, "warning", {autoClose: 3000, actions: [
+            { label: 'Go to', callback: () => {
+              currentNotebook.content.activeCellIndex = index;
+              if (currentNotebook.content.activeCell){
+                currentNotebook.content.activeCell.activate()
+                currentNotebook.content.scrollToCell(currentNotebook.content.activeCell);
+              }
+            } }
+          ],});  
+          break;
+          }
+        }
     }
-    let widget = newWidget();
-
-    const command: string = 'todo:open';
-    app.commands.addCommand(command, {
-      label: 'Random Picture',
-      execute: () => {
-        if (widget.isDisposed) {
-          widget = newWidget();
-        }
-        if (!widget.isAttached){
-          // Attach the widget to the main area
-          app.shell.add(widget,'main');
-        }
-        // Activate the widget
-        app.shell.activateById(widget.id);
-      }
+    tracker.currentChanged.connect(() => {
+      checkForTodos();
     });
-    palette.addItem({ command,category:'Tutorial'});
+    app.commands.addCommand(TODO_COMMAND, {
+      label: 'Check for TODOs',
+      execute: checkForTodos,
+      isVisible: () => tracker.currentWidget !==null
+    });
+
+    app.commands.addKeyBinding({
+      command: TODO_COMMAND,
+      keys: ['Ctrl Y'],
+      selector: '.jp-Notebook'
+    });
+    
   }
 
   
